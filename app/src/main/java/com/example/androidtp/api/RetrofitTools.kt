18139252899e68ee6fa6ff0a99f7e90f55acd2b2
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-03-20 16:14:54 +0100

    TP 6  sans popup

--- app/src/main/java/com/example/androidtp/api/RetrofitTools.kt
+++ app/src/main/java/com/example/androidtp/api/RetrofitTools.kt
@@ -10,7 +10,8 @@ class RetrofitTools {
     // Kotlin :  companion object = tout ce qui est dedans est statics
     companion object{
 
-        val BASE_URL = "http://165.232.147.139:3000/"
+        //val BASE_URL = "http://165.232.147.139:3000/"
+        val BASE_URL = "http://localhost:3000/"
 
         // L'utilitaire conversion JSON <=> Objet
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build();
